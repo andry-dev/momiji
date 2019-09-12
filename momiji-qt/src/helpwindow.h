bab8cf7f7beff189b902e8374e77ecf7a9cdc1e9
andry-dev <peketribal2@gmail.com>
2019-09-12 17:37:33 +0200

    momiji-qt: Add stub help window
    This commit provides a stub window where help files can be accessed.
    Later on I'll implement the actual help files.

---
+++ momiji-qt/src/helpwindow.h
@@ -0,0 +1,24 @@
+#ifndef HELP_WINDOW_H
+#define HELP_WINDOW_H
+
+#include <QWidget>
+#include <qwidget.h>
+
+namespace Ui
+{
+    class HelpWindow;
+}
+
+class HelpWindow : public QWidget
+{
+    Q_OBJECT
+
+  public:
+    explicit HelpWindow(QWidget* parent = nullptr);
+    ~HelpWindow();
+
+  private:
+    Ui::HelpWindow* ui;
+};
+
+#endif
