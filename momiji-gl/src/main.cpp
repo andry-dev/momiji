df9de36f56bb904a2e7c3566a7f00918f08c7c92
andry-dev <peketribal2@gmail.com>
2019-10-06 20:44:36 +0200

    All: Ignore a readability warning for clang-tidy
    momiji-qt: Supply WinMain
    momiji-gl: Supply WinMain

--- momiji-gl/src/main.cpp
+++ momiji-gl/src/main.cpp
@@ -1,6 +1,23 @@
 #include "Gui.h"
 
+#include <asl/detect_features>
+
+#ifdef ASL_WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+    #endif
+
+    #include <windows.h>
+#endif
+
 int main()
 {
     gui();
 }
+
+#ifdef ASL_WIN32
+extern "C" int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return main();
+}
+#endif
